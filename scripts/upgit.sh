b31a018be805797b018fa2a4c5793c84104e0485
jafamo <jafamo@gmail.com>
2013-11-21 16:12:15 +0000

    indexando el codigo

--- scripts/upgit.sh
+++ scripts/upgit.sh
@@ -10,31 +10,27 @@
    if [ ! -d '.git' ]; then
    	echo 'Esta carpeta no contiene un repositorio Git'
 		exit -1
-		fi
+	fi
 		 
 		 # Ahora comprobamos si se le paso algun parametro
-		 if [ $# == 0 ]; then
-		 	echo "UpToGit: ¡Error! No se le ha pasado ningún parámetro"
-				echo "uptogit fichero1 fichero2 ... ficheroN"
-					exit -1
-					else
-						# Recorremos los parametros para comprobar si son ficheros o directorios
-							for file in $*; do
-									if [ ! -e $file ]; then
-												echo "UpToGit: El archivo o directorio $file no existe"
-															exit -1
-																	fi
-																		done
-																			
-																				# Si llegamos hasta aquí, indicamos a Git los archivos a subir
-																					git add $*
-																						
-																							# Esto nos pedira el mensaje del commit
-																								echo "Introduce el mensaje del commit:"
-																									read TXT
-																										git commit -m "$TXT"
-																										 
-																										 	# Y terminamos subiendo los archivos
-																												git push origin master
-																												 
-																												 fi
+	if [ $# == 0 ]; then
+		 echo "UpToGit: ¡Error! No se le ha pasado ningún parámetro"
+		echo "uptogit fichero1 fichero2 ... ficheroN"
+		exit -1
+	else
+	# Recorremos los parametros para comprobar si son ficheros o directorios
+		for file in $*; do
+			if [ ! -e $file ]; then
+				echo "UpToGit: El archivo o directorio $file no existe"
+				exit -1
+			fi
+		done
+		# Si llegamos hasta aquí, indicamos a Git los archivos a subir
+	git add $*
+	# Esto nos pedira el mensaje del commit
+	echo "Introduce el mensaje del commit:"
+	read TXT
+	git commit -m "$TXT"
+		# Y terminamos subiendo los archivos
+	git push origin master
+	 fi
